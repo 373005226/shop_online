55e3d7324e10dd3fe6922ef4ebc9d472d7b19d8a
373005226 <373005226@qq.com>
2020-01-06 11:05:52 +0800

    修改pc大屏幕自适应

--- src/common/headers.vue
+++ src/common/headers.vue
@@ -14,34 +14,33 @@
       <el-col :span="12">
         <div class="nav-title">
           <el-row :gutter="20">
-            <el-col :span="3" :offset="6" style="position:relative;left: 0px;">
+            <el-col :span="3" :offset="6">
                 <span @mouseover="change_person(true)" @mouseout="change_person(false)">
                   <img src="../assets/img/person.png">
                 </span>
 
-                <!--    显示个人信息下拉页面-->
-                <div class="person_position" v-show="showperson">
-                  <div class="person_style">
-                    <div class="person_style_triangle">
-                    </div>
-                  </div>
-                </div>
-
-            </el-col>
-
-            <el-clo :span="3" style="position:relative;left: 0px;">
-              <span @mouseover="change_cart(true)" @mouseout="change_cart(false)">
-                  <img src="../assets/img/buy.png" >
-                  <span class="cart_nums" style="padding-left: 5px;">0</span>
-                </span>
-
-              <!--    显示购物车下拉页面-->
-              <div class="cart_position" v-show="showcart">
-                <div class="cart_style">
-                  <div class="cart_style_triangle">
+              <!--    显示个人信息下拉页面-->
+              <div class="person_position" v-show="showperson">
+                <div class="person_style">
+                  <div class="person_style_triangle">
                   </div>
                 </div>
               </div>
+            </el-col>
+
+            <el-clo :span="3">
+              <span @mouseover="change_cart(true)" @mouseout="change_cart(false)">
+                <img src="../assets/img/buy.png">
+                <span class="cart_nums" style="padding-left: 5px;">0</span>
+                <!--    显示购物车下拉页面-->
+                <div class="cart_position" v-show="showcart">
+                  <div class="cart_style">
+                    <div class="cart_style_triangle">
+                    </div>
+                  </div>
+                </div>
+              </span>
+
 
             </el-clo>
 
@@ -60,17 +59,17 @@
 <script>
   export default {
     name: "headers",
-    data(){
-      return{
-        showcart:false,
-        showperson:false
+    data() {
+      return {
+        showcart: false,
+        showperson: false
       }
     },
-    methods:{
-      change_cart(state){
+    methods: {
+      change_cart(state) {
         this.showcart = state
       },
-      change_person(state){
+      change_person(state) {
         this.showperson = state
       }
     }
@@ -99,7 +98,13 @@
     border-radius: 10px;
     padding-right: 5px
   }
-
+  /*@media (min-width: 1920px){*/
+  /*  .cart_position {*/
+  /*    position: relative;*/
+  /*    left: 335px;*/
+  /*    top: 0px;*/
+  /*  }*/
+  /*}*/
   .cart_position {
     position: relative;
     left: 335px;
@@ -124,7 +129,7 @@
     left: 192px;
   }
 
-  .person_position{
+  .person_position {
     position: relative;
     left: -30px;
     top: 0px;
