a970df0c61b2d566ab50b1f3e5bcf0c2c831a4bf
373005226 <373005226@qq.com>
2020-01-09 08:15:35 +0800

    修改了分辨率问题，改成px，不使用自适应

--- src/common/headers.vue
+++ src/common/headers.vue
@@ -1,59 +1,40 @@
 <template>
-  <div>
-    <el-row>
-      <el-col :span="12">
-        <div class="nav-title">
-          <el-row :gutter="20">
-            <el-col :span="6" :offset="8">
-              <div class="nav-title">欢迎来到小L的线上超市!</div>
-            </el-col>
-          </el-row>
+  <div class="nav-title">
+    <div style="padding-left: 260px;">
+      欢迎来到小L的店，一站式线上选购平台
+    </div>
+    <div style="margin-left: auto;margin-right: 500px;">
+      <div style="position: absolute;left: 1150px;">
+        <span @mouseover="change_person(true)" @mouseout="change_person(false)">
+          <img src="@/assets/img/person.png">
+        </span>
+
+        <div class="person_position" v-show="showperson">
+          <div class="person_style">
+            <div class="person_style_triangle">
+            </div>
+          </div>
         </div>
-      </el-col>
 
-      <el-col :span="12">
-        <div class="nav-title">
-          <el-row :gutter="20">
-            <el-col :span="3" :offset="6">
-              <div style="position:absolute;left: 500px;">
-                <span @mouseover="change_person(true)" @mouseout="change_person(false)">
-                  <img src="../assets/img/person.png">
-                </span>
-                <!--    显示个人信息下拉页面-->
-                <div class="person_position" v-show="showperson">
-                  <div class="person_style">
-                    <div class="person_style_triangle">
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </el-col>
+      </div>
 
-            <el-col :span="3">
-              <div style="position:absolute;left: 650px;">
-                <span @mouseover="change_cart(true)" @mouseout="change_cart(false)">
-                <img src="../assets/img/buy.png">
-                <span class="cart_nums" style="padding-left: 5px;">0</span>
-              </span>
-                <!--    显示购物车下拉页面-->
-                <div class="cart_position" v-show="showcart">
-                  <div class="cart_style">
-                    <div class="cart_style_triangle">
-                    </div>
-                    <p style="padding-top: 170px;color: #999999;">购物车为空哦</p>
-                  </div>
-                </div>
-              </div>
-            </el-col>
+      <div style="position:absolute;left: 1300px;">
+        <span @mouseover="change_cart(true)" @mouseout="change_cart(false)">
+          <img src="@/assets/img/buy.png">
+          <span class="cart_nums">0</span>
+        </span>
 
-          </el-row>
-
+        <div class="cart_position" v-show="showcart">
+          <div class="cart_style">
+            <div class="cart_style_triangle">
+            </div>
+            <p style="padding-top: 170px;color: #999999;">购物车为空哦</p>
+          </div>
         </div>
-      </el-col>
 
-    </el-row>
+      </div>
 
-
+    </div>
   </div>
 </template>
 
@@ -78,31 +59,30 @@
 </script>
 
 <style scoped>
-
   .nav-title {
-    line-height: 30px;
-    height: 30px;
+    line-height: 36px;
+    width: 100%;
+    /*height: 30px;*/
+    height: 36px;
     background-image: linear-gradient(#fff, #f1f1f1);
     border-bottom: 1px solid #dadada;
     border-radius: 4px;
-    min-height: 36px;
-  }
-
-  .nav-title span {
-    padding-left: 30px
+    display: flex;
+    flex-direction: row;
   }
 
   .cart_nums {
     background: #9B9B9B;
     color: white;
     border: 5px;
+    padding-left: 5px;
     border-radius: 10px;
     padding-right: 5px;
   }
 
   .cart_position {
     position: relative;
-    left: -155px;
+    left: -185px;
     top: 0px;
     z-index: 1;
     background: white;
@@ -129,7 +109,7 @@
 
   .person_position {
     position: relative;
-    left: -30px;
+    left: -60px;
     top: 0px;
     z-index: 1;
     background: white;
--- src/common/search.vue
+++ src/common/search.vue
@@ -1,33 +1,25 @@
 <template>
-    <div>
-      <el-row>
-        <el-col :span="12">
-          <el-col :offset="8">
-            <img src="../assets/img/L的小店.png" style="height: 130px;">
-          </el-col>
-        </el-col>
-
-        <el-col :span="12">
-          <el-col :span="8" style="padding-top: 47px;display: flex;flex-direction: row;">
-            <el-input v-model="input" placeholder="搜索你想要的商品" clearable  ></el-input>
-            <el-button>搜索</el-button>
-          </el-col>
-        </el-col>
-      </el-row>
+  <div style="display: flex;flex-direction: row;height: 150px;">
+      <span>
+        <img src="@/assets/img/L的小店.png" style="height: 130px;padding-left: 335px;">
+      </span>
+    <div style="display: flex;flex-direction: row;padding-top: 47px;padding-left: 100px;">
+      <el-input v-model="input" style="width: 360px;" placeholder="搜索你想要的商品" clearable ></el-input>
+      <el-button style="height: 40px;">搜索</el-button>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-      name: "search",
-      data() {
-        return {
-          input: ''
-        }
+  export default {
+    name: "search",
+    data() {
+      return {
+        input: ''
       }
     }
+  }
 </script>
 
 <style scoped>
-
-</style>
+</style>s
