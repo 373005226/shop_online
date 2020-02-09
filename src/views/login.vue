ecb5cb9501068ed9a1799f7819d9cb866f014940
373005226 <373005226@qq.com>
2020-02-09 16:56:15 +0800

    美化了登录界面

--- src/views/login.vue
+++ src/views/login.vue
@@ -7,17 +7,40 @@
             <div class="content">
               <h3>手机登录</h3>
               <div class="username">
-                <input type="text" class="usernameinput" placeholder="用户名/手机号">
+                <el-input
+                  placeholder="用户名/手机号"
+                  v-model="username"
+                  class="usernameinput"
+                  prefix-icon="el-icon-phone"
+                  clearable>
+                </el-input>
               </div>
               <div class="password">
-                <input type="text" class="passwordinout" placeholder="验证码">
-                <el-button type="primary" class="send">发送验证码</el-button>
+                <el-input placeholder="请输入密码"
+                          v-model="password"
+                          prefix-icon="el-icon-goods"
+                          show-password></el-input>
+<!--                <el-button type="primary" class="send">发送验证码</el-button>-->
               </div>
               <button type="sub mit" class="login">
                 登录
               </button>
-              <h6 class="bottom">忘记密码</h6>
-              <h6 class="bottom">注册用户</h6>
+              <h6 class="register">立即注册</h6>
+              <h6 class="forget">忘记密码</h6>
+              <div class="bottom">
+                <a href="#" class="wechat">
+                  <img src="@/assets/img/wechat.png">
+                  <span>微信登录</span>
+                </a>
+                <a href="#" class="ali">
+                  <img src="@/assets/img/ali.png">
+                  <span>支付宝登录</span>
+                </a>
+                <a href="#" class="phone">
+                  <img src="@/assets/img/phone.png">
+                  <span>手机号登录</span>
+                </a>
+              </div>
             </div>
           </form>
         </div>
@@ -31,6 +54,12 @@
   import MyFooter from "../common/footer/MyFooter";
   export default {
     name: "login",
+    data(){
+      return{
+        username:'',
+        password:''
+      }
+    },
     components:{
       MyHeader,
       MyFooter
@@ -51,7 +80,8 @@
     position: absolute;
     height: 400px;
     width: 350px;
-    background: rgba(233,233,242,0.9);
+    /*background: rgba(233,233,242,0.97);*/
+    background: white;
     right: 300px;
     top: 330px;
     box-shadow: 15px 5px 20px 1px rgba(33,34,127,0.25);
@@ -72,7 +102,6 @@
     border: 1px solid white;
   }
   .usernameinput{
-    height: 47px;
     width: 100% ;
   }
   .password{
@@ -80,11 +109,10 @@
     border: 1px solid white;
   }
   .passwordinout{
-    height: 47px;
-    width: 70%;
+    width: 100%;
   }
   input::-webkit-input-placeholder{
-    font-size: 16px;
+    font-size: 12px;
     padding-left: 10px;
     color: #999999;
   }
@@ -110,13 +138,56 @@
   .login:hover{
     background-color: #f52b00;
   }
-  .bottom{
+  .register{
+    float: left;
+    padding-top: 20px;
+    cursor: pointer;
+  }
+  .register:hover{
+    color: orange;
+  }
+  .forget{
     float: right;
     padding-top: 20px;
     padding-right: 10px ;
     cursor: pointer;
   }
-  .bottom:hover{
+  .forget:hover{
+    color: orange;
+  }
+  .bottom{
+    height: 45px;
+    width: 100%;
+    margin-top: 70px;
+    padding-top: 20px;
+  }
+  .bottom img{
+    height: 20px;
+    width: 20px;
+  }
+  .bottom span{
+    font-size: 12px;
+    padding-left: 5px;
+  }
+  .wechat{
+    border-right: 1px solid #999999;
+    padding-right: 9px;
+  }
+  .wechat:hover{
+    color: orange;
+  }
+  .ali{
+    padding-left: 10px;
+    border-right: 1px solid #999999;
+    padding-right: 9px;
+  }
+  .ali:hover{
+    color: orange;
+  }
+  .phone{
+    padding-left: 10px;
+  }
+  .phone:hover{
     color: orange;
   }
 </style>
